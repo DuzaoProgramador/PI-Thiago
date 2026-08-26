4c6c78023c334b02945f8450186b96691e990b3b
DuzaoProgramador <eduardopi2026.2@gmail.com>
2026-08-26 01:48:34 +0000

    Aula 25/08

--- PI-Thiago/src/app/app-module.ts
+++ PI-Thiago/src/app/app-module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Aluno } from './aluno/aluno';
+import { CadAluno } from './cad-aluno/cad-aluno';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Aluno, CadAluno],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
