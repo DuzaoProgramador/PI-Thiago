484af535c9c8610879417059118d10e35d66c01f
DuzaoProgramador <eduardopi2026.2@gmail.com>
2026-09-04 23:34:44 +0000

    ex2

--- PI-Thiago/src/app/lista-ex1/lista-ex1-module.ts
+++ PI-Thiago/src/app/lista-ex1/lista-ex1-module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Ex1 } from './ex1/ex1';
+import { Ex2 } from './ex2/ex2';
 
 import { ListaEx1RoutingModule } from './lista-ex1-routing-module';
 
 @NgModule({
-  declarations: [Ex1],
+  declarations: [
+    Ex1,
+    Ex2
+  ],
   imports: [
     CommonModule,
     ListaEx1RoutingModule
