de2ec0e5289dc8035f368b86ab63a9b4fb323f21
DuzaoProgramador <eduardopi2026.2@gmail.com>
2026-09-04 23:47:45 +0000

    ex4

--- PI-Thiago/src/app/lista-ex1/lista-ex1-module.ts
+++ PI-Thiago/src/app/lista-ex1/lista-ex1-module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { Ex1 } from './ex1/ex1';
 import { Ex2 } from './ex2/ex2';
 import { Ex3 } from './ex3/ex3';
+import { Ex4 } from './ex4/ex4';
 
 import { ListaEx1RoutingModule } from './lista-ex1-routing-module';
 
@@ -11,7 +12,8 @@ import { ListaEx1RoutingModule } from './lista-ex1-routing-module';
   declarations: [
     Ex1,
     Ex2,
-    Ex3
+    Ex3,
+    Ex4
   ],
   imports: [
     CommonModule,
