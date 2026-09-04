332cba493ce6d4f47d212ef64b2328a62a77a81a
DuzaoProgramador <eduardopi2026.2@gmail.com>
2026-09-04 23:52:22 +0000

    ex5 e 6

--- PI-Thiago/src/app/lista-ex1/lista-ex1-module.ts
+++ PI-Thiago/src/app/lista-ex1/lista-ex1-module.ts
@@ -5,6 +5,8 @@ import { Ex1 } from './ex1/ex1';
 import { Ex2 } from './ex2/ex2';
 import { Ex3 } from './ex3/ex3';
 import { Ex4 } from './ex4/ex4';
+import { Ex5 } from './ex5/ex5';
+import { Ex6 } from './ex6/ex6';
 
 import { ListaEx1RoutingModule } from './lista-ex1-routing-module';
 
@@ -13,7 +15,9 @@ import { ListaEx1RoutingModule } from './lista-ex1-routing-module';
     Ex1,
     Ex2,
     Ex3,
-    Ex4
+    Ex4,
+    Ex5,
+    Ex6
   ],
   imports: [
     CommonModule,
